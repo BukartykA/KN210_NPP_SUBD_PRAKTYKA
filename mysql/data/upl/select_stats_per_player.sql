a3e9d50cbb0bece9e38948671b29de5d435d9436
BukartykA <andreybukartyk2208@gmail.com>
2025-11-05 10:49:59 +0200

    task: Add aggregated stats for goals and cards per player

---
+++ mysql/data/upl/select_stats_per_player.sql
@@ -0,0 +1,20 @@
+﻿SELECT
+    -- 1. Колонка: Футболіст
+    gr.imya AS Imya,
+    gr.prizvyshche AS Prizvyshche,
+    
+    -- 2. Колонка: Кількість Голів
+    COUNT(DISTINCT g.id_gola) AS Kilkist_Goliv,
+    
+    -- 3. Колонка: Кількість Карток
+    COUNT(DISTINCT k.id_kartky) AS Kilkist_Kartok
+FROM
+    gravets gr
+LEFT JOIN
+    gol g ON gr.id_gravtsya = g.id_gravtsya
+LEFT JOIN
+    kartka k ON gr.id_gravtsya = k.id_gravtsya
+GROUP BY
+    gr.id_gravtsya, gr.imya, gr.prizvyshche
+ORDER BY
+    Kilkist_Goliv DESC, Kilkist_Kartok DESC;
